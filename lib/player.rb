b8b5c1588176e408952b54c12ce264da59dea533
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-08-30 15:43:17 -0500

    Add get_move method to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 # Description/Explanation: Creates a player object to be used in a tic-tac-toe game. The player object has a name and a
 # symbol, which is used to represent the player's moves on the board. The player object is initialized with a name and a
 # symbol, and it has read-only access to these attributes through the name and symbol methods.
@@ -9,5 +11,15 @@ class Player
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
+  end
+
+  def get_move(board)
+    print "#{@name}, enter your move (row, column): "
+    move = gets.chomp.split(',').map(&:to_i)
+    until board.valid_move?(move)
+      print 'Invalid move. Enter your move (row, column): '
+      move = gets.chomp.split(',').map(&:to_i)
+    end
+    move
   end
 end
