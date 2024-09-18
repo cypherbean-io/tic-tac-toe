88ef5ca1ef1fffc8be5eb913f38164d610650006
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-09-18 17:29:25 -0500

    Remove require pry-byebug from player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'pry-byebug'
-
 # Description/Explanation: Creates a player object to be used in a tic-tac-toe game. The player object has a name and a
 # symbol, which is used to represent the player's moves on the board. The player object is initialized with a name and a
 # symbol, and it has read-only access to these attributes through the name and symbol methods.
